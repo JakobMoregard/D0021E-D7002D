ddfcfc5566ee17806051c04607a176479692eaa4
Marcus Lund <synapz3@ludd.ltu.se>
2018-02-09 17:07:25 +0100

    Update of CBR

--- Lab 2/src/Sim/Generator_CBR.java
+++ Lab 2/src/Sim/Generator_CBR.java
@@ -7,6 +7,7 @@ import java.io.IOException;
 public class Generator_CBR extends Node {
 	
 	protected double _time = 0;
+	protected int _number_of_packages_per_second;
 	
 	public Generator_CBR (int network, int node) {
 		super(node, node);
@@ -20,82 +21,58 @@ public class Generator_CBR extends Node {
         writer.close();
     }
 	
-	public void StartSending(int network, int node, int number, int timeInterval, int startSeq, int number_of_packages_per_secound)
+	public void StartSending(int network, int node, int number, int timeInterval, int startSeq, int number_of_packages_per_second)
 	{
+        _number_of_packages_per_second = number_of_packages_per_second;
 		_stopSendingAfter = number;
 		_timeBetweenSending = timeInterval;
 		_toNetwork = network;
 		_toHost = node;
 		_seq = startSeq;
-		
-		// Prints statistics of the generator
-		System.out.println("Traffic generated from CBR_Node:"+
-		
-		"\n	Sender: " +
-		"CBR_Generator, " + this +
-		
-		"\n	Reciever: " +
-		_toHost +
-		
-		"\n	To network: " +
-		_toNetwork + 
-		
-		"\n	Time between sending: " +
-		_timeBetweenSending);
+        send(this, new TimerEvent(), 0);
+        System.out.println("Sending signal to start sending...");
 
-		double temp_time = 1.0/number_of_packages_per_secound;
-		int pkg_nr = 1;         //Prints the number of any package in the loop bellow.
-		int num_packages = 0;   //This will help to stop generating packages when you reach the limit of _stopSendingAfter
-		
-		 for (int i = 0; _stopSendingAfter > i; i++) {
-			 for (int y = 0; y < number_of_packages_per_secound; y++) {
-			     if (num_packages >=_stopSendingAfter){
-			         return;
-                 }
-
-                 try{
-                     log_time(Double.toString(_time), "CBR_Generator_Sending");
-                 } catch (Exception e) {
-                     // TODO: handle exception
-                     System.out.println(e);
-                 }
-
-				 System.out.println("Time of sending package " + pkg_nr + " is: " + _time);
-				 pkg_nr++;
-				 send(this, new TimerEvent(), _time);
-				 _time += temp_time;
-				 num_packages++;
-
-
-
-			 }
-			 _time += timeInterval;
-			 _timeBetweenSending += timeInterval;
-		 }
 	}
 	
 	public void recv(SimEnt src, Event ev)
 	{
+
+
 		if (ev instanceof TimerEvent)
 		{
 
 			if (_stopSendingAfter > _sentmsg)
 			{
-				_sentmsg++;
-				send(_peer, new Message(_id, new NetworkAddr(_toNetwork, _toHost),_seq),0);
-				send(this, new TimerEvent(),_timeBetweenSending);
-				System.out.println("Node "+_id.networkId()+ "." + _id.nodeId() +" sent message with seq: "+_seq + " at time "+SimEngine.getTime());
-				_seq++;
+
+                double temp_time = 1.0/_number_of_packages_per_second;
+                int pkg_nr = 1;         //Prints the number of any package in the loop bellow.
+                _time = 0;
+
+                for (int y = 0; y < _number_of_packages_per_second; y++) {
+
+                    try{
+                        log_time(Double.toString((SimEngine.getTime() + _time)), "CBR_Generator_Sending");
+                    } catch (Exception e) {
+                        // TODO: handle exception
+                        System.out.println(e);
+                    }
+
+                    System.out.println("Time of sending package " + pkg_nr + " is: " + (SimEngine.getTime() + _time) + "\n");
+
+                    _sentmsg++;
+                    send(_peer, new Message(_id, new NetworkAddr(_toNetwork, _toHost),_seq), _time);
+                    System.out.println("Node "+_id.networkId()+ "." + _id.nodeId() +" sent message with seq: "+_seq + " at time "+(SimEngine.getTime() + _time));
+                    _seq++;
+                    _time += temp_time;
+
+                }
+                send(this, new TimerEvent(),1);
 			}
 		}
+
 		if (ev instanceof Message)
 		{
-            try{
-                log_time(Double.toString(SimEngine.getTime()), "CBR_Generator_Recieving");
-            } catch (Exception e) {
-                // TODO: handle exception
-                System.out.println(e);
-            }
+
 			System.out.println("Node "+_id.networkId()+ "." + _id.nodeId() +" receives message with seq: "+((Message) ev).seq() + " at time "+SimEngine.getTime());
 		}
 	}
