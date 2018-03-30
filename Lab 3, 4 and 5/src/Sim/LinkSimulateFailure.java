b15e39ca3f00745869c1a340b7bc910d981885b1
Synapz3 <synapz3@encrypt.systems>
2018-03-30 15:42:44 +0200

    added a link to simulate failure

---
+++ Lab 3, 4 and 5/src/Sim/LinkSimulateFailure.java
@@ -0,0 +1,61 @@
+package Sim;
+
+public class LinkSimulateFailure extends SimEnt{
+    protected SimEnt _connectorA=null;
+    protected SimEnt _connectorB=null;
+    protected int _now=0;
+    protected int link_cost;
+    protected int failure;
+
+    public LinkSimulateFailure(int link_cost, int linkFailure)
+    {
+        super();
+        this.link_cost = link_cost;
+        failure = linkFailure;
+    }
+
+    // Connects the link to some simulation entity like
+    // a node, switch, router etc.
+
+    public void setConnector(SimEnt connectTo)
+    {
+        if (_connectorA == null)
+            _connectorA=connectTo;
+        else
+            _connectorB=connectTo;
+    }
+
+    // Called when a message enters the link
+
+    public void recv(SimEnt src, Event ev)
+    {
+
+        if (SimEngine.getTime() > failure){
+
+        } else {
+
+            if (ev instanceof Message || ev instanceof ChangeInterface)
+            {
+                System.out.println("Link recv msg, passes it through");
+                if (src == _connectorA)
+                {
+                    send(_connectorB, ev, _now);
+                }
+                else
+                {
+                    send(_connectorA, ev, _now);
+                }
+            }
+
+            // Allow RIP packages
+            if (ev instanceof RIP) {
+                System.out.println("Link recv RIP broadcast, passes it through");
+                if (src == _connectorA) {
+                    send(_connectorB, ev, _now);
+                } else {
+                    send(_connectorA, ev, _now);
+                }
+            }
+        }
+    }
+}
